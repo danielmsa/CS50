1ce94f9291e3663d12752bb1e283834cd8047fd7
Daniel Araújo <danielmsaraujo@globo.com>
2021-06-01 17:45:43 -0300

    Adding the resolution of the Lab1 - C

---
+++ Lab1/population.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n, m, y, b, d;
+    
+    // TODO: Prompt for start size
+    do
+    {
+        n = get_int("start size: ");
+    }
+    while (n < 9);
+    
+    // TODO: Prompt for end size
+    do
+    {
+        m = get_int("end size: ");
+    }
+    while (m < n);
+    
+    // TODO: Calculate number of years until we reach threshold
+    for (y = 0; n < m; y++)
+    {
+        b = n / 3;
+
+        d = n / 4;
+
+        n = n + b - d; 
+    }
+    
+    // TODO: Print number of years
+    {
+        printf("Years: %i\n", y);
+    }
+}
+
